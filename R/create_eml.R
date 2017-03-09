088e88aea42dc26241f7cfa4dfbc66de5c14129b
Colin Smith <colin.smith@wisc.edu>
2017-03-09 15:39:12 -0600

    Add patch to complete imports.

--- R/create_eml.R
+++ R/create_eml.R
@@ -24,6 +24,8 @@ create_eml <- function(path) {
   library("EML")
   library("xlsx")
   library("rmarkdown")
+  library("XML")
+  library("xml2")
 
   # Parameterize function -----------------------------------------------------
 
