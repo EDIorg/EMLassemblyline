2f3553d28ec9d86cafc109ded99ff672b824049a
Colin Smith <colin.smith@wisc.edu>
2017-05-03 10:00:22 -0500

    Correct units for <size> is "byte". Edit this.

--- R/create_eml.R
+++ R/create_eml.R
@@ -634,7 +634,7 @@ create_eml <- function(path) {
                              fieldDelimiter = field_delimeter[i])
 
     physical@size <- new("size",
-                         unit = "bytes",
+                         unit = "byte",
                          as.character(
                            file.size(
                              paste(path,
