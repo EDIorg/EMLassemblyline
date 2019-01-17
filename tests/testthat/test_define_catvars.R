a01cbe20e6f3b3079afde1f0bf0c69a0c75c7d60
clnsmth <colin.smith@wisc.edu>
2019-01-17 12:03:51 -0600

    Reduce path length

--- tests/testthat/test_define_catvars.R
+++ tests/testthat/test_define_catvars.R
@@ -4,9 +4,8 @@ library(EMLassemblyline)
 
 # Parameterize ----------------------------------------------------------------
 
-path <- system.file('/example_dataset/templates/abstract.txt', 
-                    package = 'EMLassemblyline')
-parent_dir <- substr(path, 1, nchar(path)-32)
+path <- system.file('/examples/templates/abstract.txt', package = 'EMLassemblyline')
+parent_dir <- substr(path, 1, nchar(path)-23)
 
 # Tests -----------------------------------------------------------------------
 
@@ -20,7 +19,7 @@ testthat::test_that('Error when attribute files are missing', {
 testthat::test_that('Return message when catvars file already exists', {
   output <- suppressMessages(
     define_catvars(
-      path = paste0(parent_dir, '/metadata_templates'),
+      path = paste0(parent_dir, '/templates'),
       data.path = paste0(parent_dir, '/data')
       )
   )
@@ -30,7 +29,7 @@ testthat::test_that('Return message when catvars file already exists', {
 testthat::test_that('Return data frame when files have not yet been created', {
   output <- suppressMessages(
     define_catvars(
-      path = paste0(parent_dir, '/metadata_templates_missing_catvars'),
+      path = paste0(parent_dir, '/templates_cv'),
       data.path = paste0(parent_dir, '/data')
       )
     )
