4935c535e44664598f5e954784a884a89eae3da3
Colin Smith <colin.smith@wisc.edu>
2019-01-28 17:09:11 -0600

    Begin testing of make_eml.R

---
+++ tests/testthat/test_make_eml.R
@@ -0,0 +1,198 @@
+context('Make EML')
+library(EMLassemblyline)
+library(EML)
+
+# Parameterize ----------------------------------------------------------------
+
+path <- system.file(
+  '/examples/templates/abstract.txt', 
+  package = 'EMLassemblyline'
+)
+path <- substr(path, 1, nchar(path) - 23)
+
+# Possible arguments to make_eml.R
+
+path <- paste0(path, '/templates')
+data.path <- paste0(path, '/data')
+eml.path <- paste0(path, '/eml')
+dataset.title <- 'Sphagnum and Vascular Plant Decomposition under Increasing Nitrogen Additions: 2014-2015'
+data.files <- c('decomp.csv', 'nitrogen.csv')
+data.files.description <- c('Decomposition data', 'Nitrogen data')
+temporal.coverage <- c('2014-05-01', '2015-10-31')
+geographic.description <- 'Alberta, Canada, 100 km south of Fort McMurray, Canada'
+geographic.coordinates <- c('55.895', '112.094','55.895', '112.094')
+maintenance.description <- 'completed'
+user.id <- 'csmith'
+affiliation <- 'LTER'
+package.id <- 'edi.141.1'
+data.files.quote.character <- c("\\'", "\\'")
+data.files.url <- 'https://lter.limnology.wisc.edu/sites/default/files/data/edi_141/data'
+zip.dir <- 'protocols.zip'
+zip.dir.description <- 'Collection of sampling and analytical protocols'
+provenance <- 'edi.100.1'
+
+# File paths within R library
+
+path.inst <- system.file(
+  '/examples/templates/abstract.txt', 
+  package = 'EMLassemblyline'
+)
+path.inst <- substr(path.inst, 1, nchar(path.inst)-23)
+
+# Expect errors ---------------------------------------------------------------
+
+testthat::test_that('Error out when required arguments are missing', {
+  
+  # path
+  
+  expect_error(
+    make_eml(
+      dataset.title = dataset.title,
+      temporal.coverage = temporal.coverage,
+      geographic.description = geographic.description,
+      geographic.coordinates = geographic.coordinates,
+      maintenance.description = maintenance.description,
+      user.id = user.id,
+      affiliation = affiliation
+    )
+  )
+  
+  # dataset.title
+  
+  expect_error(
+    make_eml(
+      path = path,
+      temporal.coverage = temporal.coverage,
+      geographic.description = geographic.description,
+      geographic.coordinates = geographic.coordinates,
+      maintenance.description = maintenance.description,
+      user.id = user.id,
+      affiliation = affiliation
+    )
+  )
+  
+  # temporal.coverage
+  
+  expect_error(
+    make_eml(
+      path = path,
+      dataset.title = dataset.title,
+      geographic.description = geographic.description,
+      geographic.coordinates = geographic.coordinates,
+      maintenance.description = maintenance.description,
+      user.id = user.id,
+      affiliation = affiliation
+    )
+  )
+  
+  # geographic.description
+  
+  expect_error(
+    make_eml(
+      path = path,
+      dataset.title = dataset.title,
+      temporal.coverage = temporal.coverage,
+      geographic.coordinates = geographic.coordinates,
+      maintenance.description = maintenance.description,
+      user.id = user.id,
+      affiliation = affiliation
+    )
+  )
+  
+  # geographic.corrdinates
+  
+  expect_error(
+    make_eml(
+      path = path,
+      dataset.title = dataset.title,
+      temporal.coverage = temporal.coverage,
+      geographic.description = geographic.description,
+      maintenance.description = maintenance.description,
+      user.id = user.id,
+      affiliation = affiliation
+    )
+  )
+  
+  # maintenance.description
+  
+  expect_error(
+    make_eml(
+      path = path,
+      dataset.title = dataset.title,
+      temporal.coverage = temporal.coverage,
+      geographic.description = geographic.description,
+      geographic.coordinates = geographic.coordinates,
+      user.id = user.id,
+      affiliation = affiliation
+    )
+  )
+  
+  # user.id
+  
+  expect_error(
+    make_eml(
+      path = path,
+      dataset.title = dataset.title,
+      temporal.coverage = temporal.coverage,
+      geographic.description = geographic.description,
+      geographic.coordinates = geographic.coordinates,
+      maintenance.description = maintenance.description,
+      affiliation = affiliation
+    )
+  )
+  
+  # affiliation
+  
+  expect_error(
+    make_eml(
+      path = path,
+      dataset.title = dataset.title,
+      temporal.coverage = temporal.coverage,
+      geographic.description = geographic.description,
+      geographic.coordinates = geographic.coordinates,
+      maintenance.description = maintenance.description,
+      user.id = user.id
+    )
+  )
+  
+})
+
+# Expect equal ----------------------------------------------------------------
+
+# testthat::test_that('Expect equal', {
+# 
+#   # Outputs of example dataset
+#   
+#   eml_expected <- EML::read_eml(
+#     paste0(
+#       path.inst,
+#       '/eml/edi.141.1.xml'
+#     )
+#   )
+#   
+#   eml_attempted <- make_eml(
+#     path = path,
+#     data.path = data.path,
+#     eml.path = eml.path,
+#     dataset.title = 'Sphagnum and Vascular Plant Decomposition under Increasing Nitrogen Additions: 2014-2015',
+#     data.files = data.files,
+#     data.files.description = c('Decomposition data', 'Nitrogen data'),
+#     temporal.coverage = c('2014-05-01', '2015-10-31'),
+#     geographic.description = 'Alberta, Canada, 100 km south of Fort McMurray, Canada',
+#     geographic.coordinates = c('55.895', '112.094','55.895', '112.094'),
+#     maintenance.description = 'completed',
+#     user.id = 'csmith',
+#     affiliation = 'LTER',
+#     package.id = 'edi.141.1',
+#     return.obj = TRUE
+#   )
+#   
+#   expect_equal(
+#     import_templates(
+#       path = path,
+#       license = 'CC0',
+#       data.path = data.path,
+#       data.files = c('decomp.csv', 'nitrogen.csv')
+#     )
+#   )
+# })
