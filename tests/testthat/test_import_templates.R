a9f676143f342fe88e0cbf0bc02ea838d2ab7e18
clnsmth <colin.smith@wisc.edu>
2019-01-27 20:30:58 -0600

    Test import_templates.R

---
+++ tests/testthat/test_import_templates.R
@@ -0,0 +1,68 @@
+context('Import templates')
+library(EMLassemblyline)
+
+# Parameterize ----------------------------------------------------------------
+
+path <- system.file(
+  '/examples/templates/abstract.txt', 
+  package = 'EMLassemblyline'
+)
+path <- substr(path, 1, nchar(path) - 23)
+
+data.path <- paste0(path, '/data')
+
+path <- paste0(path, '/templates')
+
+# Tests -----------------------------------------------------------------------
+
+# Errors
+
+testthat::test_that('Error out when required arguments are missing', {
+  
+  expect_error(
+    import_templates(
+      license = 'CC0',
+      data.path = data.path,
+      data.files = c('decomp.csv', 'nitrogen.csv')
+    )
+  )
+  
+  expect_error(
+    import_templates(
+      path = path,
+      data.path = data.path,
+      data.files = c('decomp.csv', 'nitrogen.csv')
+    )
+  )
+  
+  expect_error(
+    import_templates(
+      path = path,
+      license = 'CC0',
+      data.files = c('decomp.csv', 'nitrogen.csv')
+    )
+  )
+  
+  expect_error(
+    import_templates(
+      path = path,
+      license = 'CC0',
+      data.path = data.path
+    )
+  )
+  
+})
+
+# Messages
+
+testthat::test_that('Return messages when templates already exist', {
+
+  expect_message(
+    import_templates(
+      path = path,
+      license = 'CC0',
+      data.path = data.path,
+      data.files = c('decomp.csv', 'nitrogen.csv')
+    )
+  )
+})
