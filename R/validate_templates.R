e2df64d5432fca1f0c8f57aa6f972fa5b57da928
clnsmth <colin.smith@wisc.edu>
2019-03-29 17:00:09 -0500

    Begin development of validate_templates.R
    Scope of this function is to all EMLassemblyline metadata templates.

---
+++ R/validate_templates.R
@@ -0,0 +1,16 @@
+#' Validate content of `EMLassemblyline` metadata templates
+#'
+#' @description
+#'     Validate the content of `EMLassembline` metadata templates.
+#'
+#' @usage validate_arguments(x)
+#'
+#' @param x
+#'     (ADD OBJECT CLASSES) Describe objects.
+#'
+
+validate_templates <- function(x){
+  
+  
+  
+}
