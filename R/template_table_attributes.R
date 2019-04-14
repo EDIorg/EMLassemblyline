6cdc65f8e1e7ecebad283eec2a2464f4b0acfbac
Colin Smith <colin.smith@wisc.edu>
2019-04-14 17:37:48 -0500

    Beta-release template_table_attributes()
    Deprecate 1/2 of import_templates() with this function.

--- R/template_table_attributes.R
+++ R/template_table_attributes.R
@@ -1,4 +1,4 @@
-#' Import core metadata templates
+#' Create table attributes template
 #'
 #' @description  
 #'     Import template files for storage of core metadata. Some templates are 
@@ -11,21 +11,15 @@
 #'     template_table_attributes(
 #'       path,
 #'       data.path = path,
-#'       license,
 #'       data.table = NULL,
 #'       x = NULL,
-#'       write.file = TRUE,
-#'       data.files = 'deprecated'
+#'       write.file = TRUE
 #'     )
 #'
 #' @param path 
 #'     (character) Path to where the templates will be imported.
 #' @param data.path
 #'     (character) Path to where the data files are stored.
-#' @param license
-#'     (character) License under which the data will be released. Use "CC0" 
-#'     (\url{https://creativecommons.org/publicdomain/zero/1.0/}), 
-#'     or "CCBY" (\url{https://creativecommons.org/licenses/by/4.0/}).
 #' @param data.table
 #'     (character) Names of tabular data. Names of non-tabular data are
 #'     entered as arguments to `make_eml`.
@@ -34,44 +28,27 @@
 #'     Use `make_arguments()` to create `x`.
 #' @param write.file
 #'     (logical) Write template files to `path`.
-#' @param data.files
-#'     NOTE: `data.files` has been deprecated. Use `data.table` instead.
 #'
 #' @return 
 #'     \itemize{
-#'         \item{`abstract.txt`} Template for the dataset abstract.
-#'         \item{`additional_info.txt`} Template for miscellaneous dataset
-#'         information.
 #'         \item{`attributes_*.txt`} Template(s) for data table attribute 
 #'         metadata, populated with some automatically extracted metadata 
 #'         content from the data files.
-#'         \item{`bounding_boxes.txt`} Template for the dataset geographic 
-#'         bounding coordinates.
-#'         \item{`custom_units.txt`} Template for defining non-standard units 
-#'         used in the dataset.
-#'         \item{`intellectual_rights.txt`} The selected intellectual rights 
-#'         license.
-#'         \item{`keywords.txt`} Template for dataset keywords.
-#'         \item{`methods.txt`} Template for dataset methods.
-#'         \item{`personnel.txt`} Template for dataset personnel and funding 
-#'         metadata.
 #'         \item{If using `x`, then content of each above listed template file 
 #'         is added to `x` under `/x/templates/`}
 #'     }
 #'     
 #' @details 
 #'     Existing templates will not be overwritten by `template_table_attributes`.
-#'
-#' @examples
 #'     
 #' @export     
 #'     
 
-template_table_attributes <- function(path, data.path = path, license, 
-                             data.table = NULL, x = NULL, write.file = TRUE,
-                             data.files = 'deprecated'){
+template_table_attributes <- function(path, data.path = path, 
+                                      data.table = NULL, x = NULL, 
+                                      write.file = TRUE){
   
-  message('Importing metadata templates')
+  message('Creating table attributes template')
   
   # Validate arguments --------------------------------------------------------
   
@@ -129,512 +106,6 @@ template_table_attributes <- function(path, data.path = path, license,
     # Indicate files have been read
     
     data_read_2_x <- TRUE
-    
-  }
-  
-  # Import abstract.txt -------------------------------------------------------
-  
-  # If writing to file ...
-  
-  if (isTRUE(write.file)){
-    
-    # Write to path
-    
-    value <- file.copy(
-      from = system.file(
-        '/templates/abstract.txt',
-        package = 'EMLassemblyline'
-      ),
-      to = paste0(
-        path,
-        "/abstract.txt"
-      )
-    )
-    
-    # Send message
-    
-    if (isTRUE(value)){
-      message("Importing abstract.txt.")
-    } else {
-      message("abstract.txt already exists!")
-    }
-    
-    # If adding to x ...
-    
-  } else if (!exists('data_read_2_x')){
-    
-    if (any(is.na(x$template$abstract.txt$content))){
-      
-      # Add to content
-      
-      x$template$abstract.txt$content <- EML::set_TextType(
-        file = system.file(
-          '/templates/abstract.txt',
-          package = 'EMLassemblyline'
-        )
-      )
-      
-      # Send message
-      
-      message("Importing abstract.txt.")
-      
-    } else {
-      
-      message("abstract.txt already exists!")
-      
-    }
-    
-  }
-  
-  # Import additional_info.txt ------------------------------------------------
-  
-  # If writing to file ... 
-  
-  if (isTRUE(write.file)){
-    
-    # Write to path
-    
-    value <- file.copy(
-      from = system.file(
-        '/templates/additional_info.txt',
-        package = 'EMLassemblyline'
-      ),
-      to = paste0(
-        path,
-        "/additional_info.txt"
-      )
-    )
-    
-    # Send message
-    
-    if (isTRUE(value)){
-      message("Importing additional_info.txt.")
-    } else {
-      message("additional_info.txt already exists!")
-    }
-    
-    # If adding to x ...
-    
-  } else if (!exists('data_read_2_x')){
-    
-    if (any(is.na(x$template$additional_info.txt$content))){
-      
-      # Add to content
-      
-      x$template$additional_info.txt$content <- EML::set_TextType(
-        file = system.file(
-          '/templates/additional_info.txt',
-          package = 'EMLassemblyline'
-        )
-      )
-      
-      # Send message
-      
-      message("Importing additional_info.txt")
-      
-    } else {
-      
-      message("additional_info.txt already exists!")
-      
-    }
-    
-  }
-  
-  # Import bounding_boxes.txt -------------------------------------------------
-  
-  # If writing to file ...
-  
-  if (isTRUE(write.file)){
-    
-    # Write to path
-    
-    value <- file.copy(
-      from = system.file(
-        '/templates/bounding_boxes.txt',
-        package = 'EMLassemblyline'
-      ),
-      to = paste0(
-        path,
-        "/bounding_boxes.txt"
-      )
-    )
-    
-    # Send message
-    
-    if (isTRUE(value)){
-      message("Importing bounding_boxes.txt.")
-    } else {
-      message("bounding_boxes.txt already exists!")
-    }
-    
-    # If adding to x ...
-    
-  } else if (!exists('data_read_2_x')){
-    
-    if (any(is.na(x$template$bounding_boxes.txt$content))){
-      
-      # Add to content
-      
-      x$template$bounding_boxes.txt$content <- utils::read.table(
-        file = system.file(
-          '/templates/bounding_boxes.txt',
-          package = 'EMLassemblyline'
-        ), 
-        header = T,
-        sep = '\t',
-        as.is = T
-      )
-      
-      # Send message
-      
-      message("Importing bounding_boxes.txt.")
-      
-    } else {
-      
-      message("bounding_boxes.txt already exists!")
-      
-    }
-    
-  }
-  
-  # Import custom_units.txt ---------------------------------------------------
-  
-  # If writing to file ...
-  
-  if (isTRUE(write.file)){
-    
-    # Write to path
-    
-    value <- file.copy(
-      from = system.file(
-        '/templates/custom_units.txt',
-        package = 'EMLassemblyline'
-      ),
-      to = paste0(
-        path,
-        "/custom_units.txt"
-      )
-    )
-    
-    # Send message
-    
-    if (isTRUE(value)){
-      message("Importing custom_units.txt.")
-    } else {
-      message("custom_units.txt already exists!")
-    }
-    
-    # If adding to x ...
-    
-  } else if (!exists('data_read_2_x')){
-    
-    if (any(is.na(x$template$custom_units.txt$content))){
-      
-      # Add to content
-      
-      x$template$custom_units.txt$content <- utils::read.table(
-        file = system.file(
-          '/templates/custom_units.txt',
-          package = 'EMLassemblyline'
-        ), 
-        header = T,
-        sep = '\t',
-        as.is = T
-      )
-      
-      # Send message
-      
-      message("Importing custom_units.txt.")
-      
-    } else {
-      
-      message("custom_units.txt already exists!")
-      
-    }
-    
-  }
-  
-  # Import intellectual_rights.txt --------------------------------------------
-  
-  # If license is CC0 ...
-  
-  if (tolower(license) == "cc0"){
-    
-    # If writing to file ...
-    
-    if (isTRUE(write.file)){
-      
-      # Write to path
-      
-      value <- file.copy(
-        from = system.file(
-          '/templates/intellectual_rights_cc0.txt',
-          package = 'EMLassemblyline'
-        ),
-        to = paste0(
-          path,
-          "/intellectual_rights.txt"
-        )
-      )
-      
-      # Send message
-      
-      if (isTRUE(value)){
-        message("Importing intellectual_rights.txt.")
-      } else {
-        message("intellectual_rights.txt already exists!")
-      }
-      
-      # If adding x ...
-      
-    } else if (!exists('data_read_2_x')){
-      
-      if (any(is.na(x$template$intellectual_rights.txt$content))){
-        
-        # Add to content
-        
-        x$template$intellectual_rights.txt$content <- EML::set_TextType(
-          file = system.file(
-            '/templates/intellectual_rights_cc0.txt',
-            package = 'EMLassemblyline'
-          )
-        )
-        
-        # Send message
-        
-        message("Importing intellectual_rights.txt.")
-        
-      } else {
-        
-        message("intellectual_rights.txt. already exists!")
-        
-      }
-      
-    }
-    
-    # If license is CCBY ...
-    
-  } else if (tolower(license) == "ccby"){
-    
-    # If writing to file ...
-    
-    if (isTRUE(write.file)){
-      
-      # Write to path
-      
-      value <- file.copy(
-        from = system.file(
-          '/templates/intellectual_rights_ccby4.0.txt',
-          package = 'EMLassemblyline'
-        ),
-        to = paste0(
-          path,
-          "/intellectual_rights.txt"
-        )
-      )
-      
-      # Send message
-      
-      if (isTRUE(value)){
-        message("Importing intellectual_rights.txt.")
-      } else {
-        message("intellectual_rights.txt already exists!")
-      }
-      
-      # If adding to x ...
-      
-    } else if (!exists('data_read_2_x')){
-      
-      if (any(is.na(x$template$intellectual_rights.txt$content))){
-        
-        # Add to content
-        
-        x$template$intellectual_rights.txt$content <- EML::set_TextType(
-          file = system.file(
-            '/templates/intellectual_rights_ccby4.0.txt',
-            package = 'EMLassemblyline'
-          )
-        )
-        
-        # Send message
-        
-        message("Importing intellectual_rights.txt.")
-        
-      } else {
-        
-        message("intellectual_rights.txt already exists!")
-        
-      }
-      
-    }
-    
-  }
-  
-  # Import keywords.txt -------------------------------------------------------
-  
-  # If writing to file ...
-  
-  if (isTRUE(write.file)){
-    
-    # Write to path
-    
-    value <- file.copy(
-      from = system.file(
-        '/templates/keywords.txt',
-        package = 'EMLassemblyline'
-      ),
-      to = paste0(
-        path,
-        "/keywords.txt"
-      )
-    )
-    
-    # Send message
-    
-    if (isTRUE(value)){
-      message("Importing keywords.txt.")
-    } else {
-      message("keywords.txt already exists!")
-    }
-    
-    # If adding to x ...
-    
-  } else if (!exists('data_read_2_x')){
-    
-    if (any(is.na(x$template$keywords.txt$content))){
-      
-      # Add to content
-      
-      x$template$keywords.txt$content <- utils::read.table(
-        file = system.file(
-          '/templates/keywords.txt',
-          package = 'EMLassemblyline'
-        ), 
-        header = T,
-        sep = '\t',
-        as.is = T
-      )
-      
-      # Send message
-      
-      message("Importing keywords.txt.")
-      
-    } else {
-      
-      message("keywords.txt already exists!")
-      
-    }
-    
-  }
-  
-  # Import methods.txt --------------------------------------------------------
-  
-  # If writing to file ...
-  
-  if (isTRUE(write.file)){
-    
-    # Write to path
-    
-    value <- file.copy(
-      from = system.file(
-        '/templates/methods.txt',
-        package = 'EMLassemblyline'
-      ),
-      to = paste0(
-        path,
-        "/methods.txt"
-      )
-    )
-    
-    # Send message
-    
-    if (isTRUE(value)){
-      message("Importing methods.txt.")
-    } else {
-      message("methods.txt already exists!")
-    }
-    
-    # If adding to x ...
-    
-  } else if (!exists('data_read_2_x')){
-    
-    if (any(is.na(x$template$methods.txt$content))){
-      
-      # Add to content
-      
-      x$template$methods.txt$content <- EML::set_methods(
-        methods_file = system.file(
-          '/templates/methods.txt',
-          package = 'EMLassemblyline'
-        )
-      )
-      
-      # Send message
-      
-      message("Importing methods.txt.")
-      
-    } else {
-      
-      message("methods.txt already exists!")
-      
-    }
-    
-  }
-  
-  # Import personnel.txt ------------------------------------------------------
-  
-  # If writing to file ...
-  
-  if (isTRUE(write.file)){
-    
-    # Write to path
-    
-    value <- file.copy(
-      from = system.file(
-        '/templates/personnel.txt',
-        package = 'EMLassemblyline'
-      ),
-      to = paste0(
-        path,
-        "/personnel.txt"
-      )
-    )
-    
-    # Send message
-    
-    if (isTRUE(value)){
-      message("Importing personnel.txt.")
-    } else {
-      message("personnel.txt already exists!")
-    }
-    
-    # If adding to x ...
-    
-  } else if (!exists('data_read_2_x')){
-    
-    if (any(is.na(x$template$personnel.txt$content))){
-      
-      # Add to content
-      
-      x$template$personnel.txt$content <- utils::read.table(
-        file = system.file(
-          '/templates/personnel.txt',
-          package = 'EMLassemblyline'
-        ), 
-        header = T,
-        sep = '\t',
-        as.is = T
-      )
-      
-      # Send message
-      
-      message("Importing personnel.txt.")
-      
-    } else {
-      
-      message("personnel.txt already exists!")
-      
-    }
     
   }
   
@@ -781,7 +252,7 @@ template_table_attributes <- function(path, data.path = path, license,
           
           message(
             paste0(
-              "Importing attributes_",
+              "Creating attributes_",
               substr(data.table[i], 1, nchar(data.table[i]) - 4),
               ".txt."
             )
@@ -831,7 +302,7 @@ template_table_attributes <- function(path, data.path = path, license,
           
           message(
             paste0(
-              "Importing attributes_",
+              "Creating attributes_",
               substr(data.table[i], 1, nchar(data.table[i]) - 4),
               ".txt."
             )
--- tests/testthat/test_template_table_attributes.R
+++ tests/testthat/test_template_table_attributes.R
@@ -1,4 +1,4 @@
-context('Template table attributes')
+context('Create table attributes template')
 library(EMLassemblyline)
 
 # Parameterize ----------------------------------------------------------------
@@ -14,90 +14,6 @@ attr_templates <- utils::read.table(
   sep = '\t',
   as.is = T
 )
-
-# File inputs = no data tables ------------------------------------------------
-
-testthat::test_that('File inputs = no data tables', {
-  
-  # Missing path results in error
-  
-  expect_error(
-    suppressMessages(
-      template_table_attributes(
-        license = 'CC0',
-        write.file = FALSE
-      )
-    )
-  )
-  
-  # Missing license results in error
-  
-  expect_error(
-    suppressMessages(
-      template_table_attributes(
-        path = system.file(
-          '/examples',
-          package = 'EMLassemblyline'
-        ),
-        write.file = FALSE
-      )
-    )
-  )
-  
-  # Unsupported license results in error
-  
-  expect_error(
-    suppressMessages(
-      template_table_attributes(
-        path = system.file(
-          '/examples',
-          package = 'EMLassemblyline'
-        ),
-        license = 'CCzero',
-        write.file = FALSE
-      )
-    )
-  )
-  
-  # New imports result in messages
-  
-  expect_message(
-    template_table_attributes(
-      path = system.file(
-        '/examples',
-        package = 'EMLassemblyline'
-      ),
-      license = 'CC0',
-      write.file = FALSE
-    )
-  )
-  
-  expect_message(
-    template_table_attributes(
-      path = system.file(
-        '/examples',
-        package = 'EMLassemblyline'
-      ),
-      license = 'CCBY',
-      write.file = FALSE
-    )
-  )
-  
-  # Attempt to import templates when they already exist results in messages
-  
-  expect_message(
-    template_table_attributes(
-      path = system.file(
-        '/examples/templates',
-        package = 'EMLassemblyline'
-      ),
-      license = 'CC0',
-      write.file = FALSE
-    )
-  )
-  
-})
-
 
 # File inputs = two data tables -----------------------------------------------
 
@@ -116,52 +32,6 @@ testthat::test_that('Test usage with file inputs', {
           'decomp.csv',
           'nitrogen.csv'
         ),
-        license = 'CC0',
-        write.file = FALSE
-      )
-    )
-  )
-  
-  # Missing license results in error
-  
-  expect_error(
-    suppressMessages(
-      template_table_attributes(
-        path = system.file(
-          '/examples',
-          package = 'EMLassemblyline'
-        ),
-        data.path = system.file(
-          '/examples/data',
-          package = 'EMLassemblyline'
-        ),
-        data.table = c(
-          'decomp.csv',
-          'nitrogen.csv'
-        ),
-        write.file = FALSE
-      )
-    )
-  )
-  
-  # Unsupported license results in error
-  
-  expect_error(
-    suppressMessages(
-      template_table_attributes(
-        path = system.file(
-          '/examples',
-          package = 'EMLassemblyline'
-        ),
-        data.path = system.file(
-          '/examples/data',
-          package = 'EMLassemblyline'
-        ),
-        data.table = c(
-          'decomp.csv',
-          'nitrogen.csv'
-        ),
-        license = 'CCzero',
         write.file = FALSE
       )
     )
@@ -180,7 +50,6 @@ testthat::test_that('Test usage with file inputs', {
           'decomp.csv',
           'nitrogen.csv'
         ),
-        license = 'CC0',
         write.file = FALSE
       )
     )
@@ -203,7 +72,6 @@ testthat::test_that('Test usage with file inputs', {
           'decompppp.csv',
           'nitrogennnnn.csv'
         ),
-        license = 'CC0',
         write.file = FALSE
       )
     )
@@ -226,7 +94,6 @@ testthat::test_that('Test usage with file inputs', {
         'decomp.csv',
         'nitrogen.csv'
       ),
-      license = 'CC0',
       write.file = FALSE
     )
   )
@@ -247,132 +114,6 @@ testthat::test_that('Test usage with file inputs', {
         'decomp.csv',
         'nitrogen.csv'
       ),
-      license = 'CC0',
-      write.file = FALSE
-    )
-  )
-  
-})
-
-# x inputs = NULL -------------------------------------------------------------
-
-testthat::test_that('x inputs = NULL', {
-  
-  # Make function call
-  
-  x <- make_arguments()
-  
-  x <- x$x
-  
-  # Missing path results in messages
-  
-  expect_message(
-    template_table_attributes(
-      license = 'CC0',
-      x = x,
-      write.file = FALSE
-    )
-  )
-  
-  # Missing path results in expected content classes with empty values
-  
-  output <- suppressMessages(
-    template_table_attributes(
-      license = 'CC0',
-      x = x,
-      write.file = FALSE
-    )
-  )
-  
-  for (i in 1:length(output$template)){
-    
-    expect_equal(
-      class(output$template[[i]]$content)[1] %in% 
-        c('TextType', 'data.frame', 'methods', 'character'),
-      TRUE
-    )
-    
-  }
-  
-  # Missing license results in error
-  
-  expect_error(
-    suppressMessages(
-      template_table_attributes(
-        path = system.file(
-          '/examples',
-          package = 'EMLassemblyline'
-        ),
-        x = x,
-        write.file = FALSE
-      )
-    )
-  )
-  
-  # Unsupported license results in error
-  
-  expect_error(
-    suppressMessages(
-      template_table_attributes(
-        path = system.file(
-          '/examples',
-          package = 'EMLassemblyline'
-        ),
-        x = x,
-        license = 'CCzero',
-        write.file = FALSE
-      )
-    )
-  )
-  
-  # Valid path results in messages
-  
-  expect_message(
-    template_table_attributes(
-      path = system.file(
-        '/examples',
-        package = 'EMLassemblyline'
-      ),
-      x = x,
-      license = 'CC0',
-      write.file = FALSE
-    )
-  )
-  
-  # Valid path results in expected content classes with empty values
-  
-  output <- suppressMessages(
-    template_table_attributes(
-      path = system.file(
-        '/examples',
-        package = 'EMLassemblyline'
-      ),
-      license = 'CC0',
-      x = x,
-      write.file = FALSE
-    )
-  )
-  
-  for (i in 1:length(output$template)){
-    
-    expect_equal(
-      class(output$template[[i]]$content)[1] %in% 
-        c('TextType', 'data.frame', 'methods', 'character'),
-      TRUE
-    )
-    
-  }
-  
-  # Attempt to import templates when they already exist results in messages
-  
-  expect_message(
-    template_table_attributes(
-      path = system.file(
-        '/examples/templates',
-        package = 'EMLassemblyline'
-      ),
-      x = output,
-      license = 'CC0',
       write.file = FALSE
     )
   )
@@ -398,11 +139,10 @@ testthat::test_that('x inputs = data tables', {
   
   x <- x$x
   
-  # Missing path and data path results in messages
+  # Missing path results in messages
   
   expect_message(
     template_table_attributes(
-      license = 'CC0',
       x = x,
       write.file = FALSE
     )
@@ -416,7 +156,6 @@ testthat::test_that('x inputs = data tables', {
         '/examples/data',
         package = 'EMLassemblyline'
       ),
-      license = 'CC0',
       x = x,
       write.file = FALSE
     )
@@ -430,40 +169,8 @@ testthat::test_that('x inputs = data tables', {
         '/examples',
         package = 'EMLassemblyline'
       ),
-      license = 'CC0',
       x = x,
       write.file = FALSE
-    )
-  )
-  
-  # Missing license results in error
-  
-  expect_error(
-    suppressMessages(
-      template_table_attributes(
-        path = system.file(
-          '/examples',
-          package = 'EMLassemblyline'
-        ),
-        x = x,
-        write.file = FALSE
-      )
-    )
-  )
-  
-  # Unsupported license results in error
-  
-  expect_error(
-    suppressMessages(
-      template_table_attributes(
-        path = system.file(
-          '/examples',
-          package = 'EMLassemblyline'
-        ),
-        x = x,
-        license = 'CCzero',
-        write.file = FALSE
-      )
     )
   )
   
@@ -480,7 +187,6 @@ testthat::test_that('x inputs = data tables', {
         'nitrogen.csv'
       ),
       x = x,
-      license = 'CC0',
       write.file = FALSE
     )
   )
@@ -499,7 +205,6 @@ testthat::test_that('x inputs = data tables', {
         'nitrogen.csv'
       ),
       x = x,
-      license = 'CC0',
       write.file = FALSE
     )
   )
@@ -555,203 +260,8 @@ testthat::test_that('x inputs = data tables', {
         'nitrogen.csv'
       ),
       x = output,
-      license = 'CC0',
       write.file = FALSE
     )
   )
   
 })
-
-# x inputs = data tables and templates ----------------------------------------
-
-testthat::test_that('x inputs = data tables and templates', {
-  
-  # Make function call
-  
-  x <- make_arguments(
-    path = system.file(
-      '/examples/templates',
-      package = 'EMLassemblyline'
-    ),
-    data.path = system.file(
-      '/examples/data',
-      package = 'EMLassemblyline'
-    ),
-    data.table = c(
-      'decomp.csv',
-      'nitrogen.csv'
-    )
-  )
-  
-  x <- x$x
-  
-  # Remove attributes_*txt templates
-  
-  x$template$attributes_decomp.txt <- NULL
-  
-  x$template$attributes_nitrogen.txt <- NULL
-  
-  # Missing path and data path results in messages
-  
-  expect_message(
-    template_table_attributes(
-      license = 'CC0',
-      x = x,
-      write.file = FALSE
-    )
-  )
-  
-  # Missing path results in messages
-  
-  expect_message(
-    template_table_attributes(
-      data.path = system.file(
-        '/examples/data',
-        package = 'EMLassemblyline'
-      ),
-      license = 'CC0',
-      x = x,
-      write.file = FALSE
-    )
-  )
-  
-  # Missing data path results in messages
-  
-  expect_message(
-    template_table_attributes(
-      path = system.file(
-        '/examples',
-        package = 'EMLassemblyline'
-      ),
-      license = 'CC0',
-      x = x,
-      write.file = FALSE
-    )
-  )
-  
-  # Missing license results in error
-  
-  expect_error(
-    suppressMessages(
-      template_table_attributes(
-        path = system.file(
-          '/examples',
-          package = 'EMLassemblyline'
-        ),
-        x = x,
-        write.file = FALSE
-      )
-    )
-  )
-  
-  # Unsupported license results in error
-  
-  expect_error(
-    suppressMessages(
-      template_table_attributes(
-        path = system.file(
-          '/examples',
-          package = 'EMLassemblyline'
-        ),
-        x = x,
-        license = 'CCzero',
-        write.file = FALSE
-      )
-    )
-  )
-  
-  # Valid data path and data tables results in messages
-  
-  expect_message(
-    template_table_attributes(
-      data.path = system.file(
-        '/examples/data',
-        package = 'EMLassemblyline'
-      ),
-      data.table = c(
-        'decomp.csv',
-        'nitrogen.csv'
-      ),
-      x = x,
-      license = 'CC0',
-      write.file = FALSE
-    )
-  )
-  
-  # Valid data path and data tables result in addition of attributes templates
-  # with expected names, class, column names, and nrows > 1
-  
-  output <- suppressMessages(
-    template_table_attributes(
-      data.path = system.file(
-        '/examples/data',
-        package = 'EMLassemblyline'
-      ),
-      data.table = c(
-        'decomp.csv',
-        'nitrogen.csv'
-      ),
-      x = x,
-      license = 'CC0',
-      write.file = FALSE
-    )
-  )
-  
-  attr_names <- paste0(
-    'attributes_',
-    stringr::str_remove(
-      string = names(output$data.table),
-      pattern = '.csv'
-    ),
-    '.txt'
-  )
-  
-  for (i in 1:length(attr_names)){
-    
-    expect_equal(
-      attr_names[i] %in% names(output$template),
-      TRUE
-    )
-    
-    expect_equal(
-      class(output$template[[attr_names[i]]]$content),
-      'data.frame'
-    )
-    
-    expect_equal(
-      all(
-        colnames(output$template[[attr_names[i]]]$content) %in%
-          c('attributeName', 'attributeDefinition', 'class', 'unit', 
-            'dateTimeFormatString', 'missingValueCode', 
-            'missingValueCodeExplanation')
-      ),
-      TRUE
-    )
-    
-    expect_equal(
-      nrow(output$template[[attr_names[i]]]$content) > 1,
-      TRUE
-    )
-    
-  }
-  
-  # Attempt to import templates when they already exist results in messages
-  
-  expect_message(
-    template_table_attributes(
-      data.path = system.file(
-        '/examples/data',
-        package = 'EMLassemblyline'
-      ),
-      data.table = c(
-        'decomp.csv',
-        'nitrogen.csv'
-      ),
-      x = output,
-      license = 'CC0',
-      write.file = FALSE
-    )
-  )
-  
-})
-
