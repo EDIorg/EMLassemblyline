429853e7ece3e3c35423dbaf84c4f39c4755030b
clnsmth <colin.smith@wisc.edu>
2020-01-25 18:16:21 -0600

    Begin implementing support for EML annotation

---
+++ R/template_annotations.R
@@ -0,0 +1,437 @@
+#' Create annotations template
+#'
+#' @description  
+#'     Use this function to extract annotatable elements from completed 
+#'     metadata templates.
+#'     \href{https://ediorg.github.io/EMLassemblyline/articles/edit_metadata_templates.html}{Instructions for editing this template.}
+#'
+#' @usage 
+#'     template_annotations(
+#'       path,
+#'       write.file = TRUE,
+#'       x = NULL
+#'     )
+#'
+#' @param path 
+#'     (character) Path to the metadata template directory.
+#' @param write.file
+#'     (logical; optional) Whether to write the template file.
+#' @param x
+#'     (named list; optional) Alternative input to 
+#'     \code{template_annotations()}. Use \code{template_arguments()} 
+#'     to create \code{x}.
+#'
+#' @return 
+#'     \strong{annotations.txt} The tab delimited annotations template. This 
+#'     file is written to \code{path} unless using \code{x}, in which case the 
+#'     template is added to \strong{/x/templates/annotations.txt}.
+#'     
+#' @details 
+#'     An existing annotations template will not be overwritten by subsequent 
+#'     calls to \code{template_annotations()}.
+#'     
+#' @examples 
+#' # Initialize data package directory for template_annotations()
+#' file.copy(
+#'  from = system.file('/examples/pkg_260', package = 'EMLassemblyline'),
+#'  to = tempdir(),
+#'  recursive = TRUE
+#' )
+#' 
+#' # Set working directory
+#' setwd(paste0(tempdir(), '/pkg_260'))
+#' 
+#' # View directory contents (NOTE: annotations.txt dosn't exist)
+#' dir('./metadata_templates')
+#' 
+#' # Template annotations
+#' template_annotations(
+#'   path = './metadata_templates'
+#' )
+#' 
+#' # View directory contents (NOTE: annotations.txt exists)
+#' dir('./metadata_templates')
+#' 
+#' # View template contents
+#' df <- data.table::fread(./metadata_templates/annotations.txt)
+#' df
+#' 
+#' # Rerunning template_annotations() does not overwrite files
+#' template_annotations(
+#'   path = './metadata_templates'
+#' )
+#' 
+#' # Clean up
+#' unlink('.', recursive = TRUE)
+#'     
+#' @export     
+#'     
+
+template_annotations <- function(
+  path,
+  write.file = TRUE,
+  x = NULL) {
+  
+  message('Templating annotations ...')
+  
+  # Validate arguments --------------------------------------------------------
+  
+  # Validate path usage before passing arguments to validate_arguments()
+  # When not using x, inputs are expected from path and data.path. When using x, 
+  # only data.path is required.
+  if (is.null(x) & missing(path)){
+    stop('Input argument "path" is missing.')
+  } else if (!is.null(x) & missing(path)){
+    path <- NULL
+    if (missing(data.path)){
+      data.path <- NULL
+    }
+  }
+  
+  # Pass remaining arguments to validate_arguments()
+  validate_arguments(
+    fun.name = 'template_table_attributes',
+    fun.args = as.list(environment())
+  )
+  
+  # Read metadata templates and data ------------------------------------------
+  # Create x if it doesn't exist and indicate it has been created (i.e. 
+  # "data_read_2_x <- TRUE").
+  
+  if (is.null(x)){
+    if (is.null(data.table)){
+      x <- template_arguments()
+      x <- x$x
+    } else if (!is.null(data.table)){
+      x <- template_arguments(
+        data.path = data.path,
+        data.table = data.table
+      )
+      x <- x$x
+    }
+    data_read_2_x <- TRUE
+  }
+  
+  # Load annotation search parameters -----------------------------------------
+  
+  
+  
+  # Create annotations.txt ----------------------------------------------------
+  
+  if (!is.null(data.table)){
+    
+    # Validate column names
+    
+    for (i in 1:length(data.table)){
+      
+      column_names <- colnames(x$data.table[[i]]$content)
+      
+      use_i <- stringr::str_detect(
+        string = column_names,
+        pattern = "\\."
+      )
+      
+      if (sum(use_i) > 0){
+        stop(
+          paste(
+            "Invalid column names detected in ", 
+            names(x$data.table)[i],
+            ":  ",
+            paste(
+              column_names[use_i], 
+              collapse = ", "
+            ), 
+            '  Replace characters located at periods "." in the above listed column names with underscores "_"',
+            sep = ""
+          )
+        )
+      }
+      
+    }
+    
+    # Extract attributes of each data file
+    
+    attributes <- list()
+    
+    for (i in 1:length(data.table)){
+      
+      # Initialize attribute table
+      
+      rows <- ncol(x$data.table[[i]]$content)
+      
+      attributes[[i]] <- data.frame(
+        attributeName = character(rows),
+        attributeDefinition = character(rows),
+        class = character(rows),
+        unit = character(rows),
+        dateTimeFormatString = character(rows),
+        missingValueCode = character(rows),
+        missingValueCodeExplanation = character(rows),
+        stringsAsFactors = FALSE
+      )
+      
+      # Get names
+      
+      attributes[[i]]$attributeName <- colnames(x$data.table[[i]]$content)
+      
+      # Guess character and numeric classes
+      
+      guess <- unname(unlist(lapply(x$data.table[[i]]$content, class)))
+      
+      guess_map <- c(
+        character = "character", 
+        logical = "character", 
+        factor = "character",
+        integer = "numeric",
+        integer64 = "numeric",
+        numeric = "numeric"
+      )
+      
+      guess <- unname(guess_map[guess])
+      
+      # Guess Date class
+      
+      use_i <- guess == "character"
+      
+      if (sum(use_i) > 0){
+        potential_date_cols <- colnames(x$data.table[[i]]$content)[use_i]
+        potential_date_i <- stringr::str_detect(tolower(potential_date_cols), "date|time|day")
+        guess_datetime <- potential_date_cols[potential_date_i]
+        use_i <- match(guess_datetime, attributes[[i]]$attributeName)
+        guess[use_i] <- "Date"
+      }
+      
+      use_i <- guess == "numeric"
+      
+      if (sum(use_i) > 0){
+        potential_date_cols <- colnames(x$data.table[[i]]$content)[use_i]
+        potential_date_i <- stringr::str_detect(tolower(potential_date_cols), "date|time|day|year")
+        guess_datetime <- potential_date_cols[potential_date_i]
+        use_i <- match(guess_datetime, attributes[[i]]$attributeName)
+        guess[use_i] <- "Date"
+      }
+      
+      # Guess factor class
+      
+      use_i <- guess == "character"
+      if (sum(use_i) > 0){
+        potential_fact_cols <- colnames(x$data.table[[i]]$content)[use_i]
+        use_i2 <- match(potential_fact_cols, colnames(x$data.table[[i]]$content))
+        if (length(use_i2) == 1){
+          unique_lengths <- length(unique(x$data.table[[i]]$content[ ,use_i2]))
+        } else {
+          unique_lengths <- apply(x$data.table[[i]]$content[ ,use_i2], 2, function(x)length(unique(x)))
+        }
+        potential_facts <- unique_lengths <= dim(x$data.table[[i]]$content)[1]*0.3
+        if (sum(potential_facts) > 0){
+          potential_facts <- names(potential_facts[potential_facts == TRUE])
+          use_i <- match(potential_facts, attributes[[i]]$attributeName)
+          guess[use_i] <- "categorical"
+        }
+      }
+      
+      # Update attributes class
+      
+      attributes[[i]]$class <- guess
+      
+      # Add unit for numeric data
+      
+      use_i <- attributes[[i]]$class == "numeric"
+      
+      if (sum(use_i) > 0){
+        attributes[[i]]$unit[use_i] <- "!Add units here!"
+      }
+      
+      # Add date time format strings for Date data
+      
+      use_i <- attributes[[i]]$class == "Date"
+      
+      if (sum(use_i) > 0){
+        attributes[[i]]$dateTimeFormatString[use_i] <- "!Add datetime specifier here!"
+      }
+      
+      # Write template to file or add template to x$template$attributes_*.txt$content
+      
+      # If writing to file ...
+      
+      if (isTRUE(write.file)){
+        
+        value <- file.exists(
+          paste0(
+            path,
+            "/",
+            "attributes_",
+            substr(data.table[i], 1, nchar(data.table[i]) - 4),
+            ".txt"
+          )
+        )
+        
+        if (!isTRUE(value)){
+          
+          message(
+            paste0(
+              "attributes_",
+              substr(data.table[i], 1, nchar(data.table[i]) - 4),
+              ".txt."
+            )
+          )
+          
+          utils::write.table(
+            attributes[[i]],
+            paste0(
+              path,
+              "/",
+              "attributes_",
+              substr(data.table[i], 1, nchar(data.table[i]) - 4),
+              ".txt"
+            ),
+            sep = "\t",
+            row.names = F,
+            quote = F,
+            fileEncoding = "UTF-8"
+          )
+          
+        } else {
+          
+          message(
+            paste0(
+              "attributes_",
+              substr(data.table[i], 1, nchar(data.table[i]) - 4),
+              ".txt already exists!"
+            )
+          )
+          
+        }
+        
+        # If adding template to x ...
+        
+      } else if (!exists('data_read_2_x')){
+        
+        value <- stringr::str_detect(
+          paste0(
+            "attributes_",
+            substr(data.table[i], 1, nchar(data.table[i]) - 4),
+            ".txt"
+          ),
+          names(x$template)
+        )
+        
+        if (!any(value)){
+          
+          message(
+            paste0(
+              "attributes_",
+              substr(data.table[i], 1, nchar(data.table[i]) - 4),
+              ".txt."
+            )
+          )
+          
+          missing_template <- list(
+            content = attributes[[i]]
+          )
+          
+          missing_template <- list(
+            missing_template
+          )
+          
+          names(missing_template) <- paste0(
+            'attributes_', 
+            substr(data.table[i], 1, nchar(data.table[i]) - 4), 
+            '.txt'
+          )
+          
+          x$template <- c(
+            x$template, 
+            missing_template
+          )
+          
+        } else {
+          
+          message(
+            paste0(
+              "attributes_",
+              substr(data.table[i], 1, nchar(data.table[i]) - 4),
+              ".txt already exists!"
+            )
+          )
+          
+        }
+        
+      }
+      
+    }
+    
+  }
+  
+  # Import custom_units.txt ---------------------------------------------------
+  
+  # If writing to file ...
+  
+  if (isTRUE(write.file)){
+    
+    # Write to path
+    
+    value <- file.copy(
+      from = system.file(
+        '/templates/custom_units.txt',
+        package = 'EMLassemblyline'
+      ),
+      to = paste0(
+        path,
+        "/custom_units.txt"
+      )
+    )
+    
+    # Send message
+    
+    if (isTRUE(value)){
+      message("custom_units.txt")
+    } else {
+      message("custom_units.txt already exists!")
+    }
+    
+    # If adding to x ...
+    
+  } else if (!exists('data_read_2_x')){
+    
+    if (any(is.na(x$template$custom_units.txt$content))){
+      
+      # Add to content
+      
+      x$template$custom_units.txt$content <- utils::read.table(
+        file = system.file(
+          '/templates/custom_units.txt',
+          package = 'EMLassemblyline'
+        ), 
+        header = T,
+        sep = '\t',
+        as.is = T
+      )
+      
+      # Send message
+      
+      message("custom_units.txt")
+      
+    } else {
+      
+      message("custom_units.txt already exists!")
+      
+    }
+    
+  }
+  
+  # Return --------------------------------------------------------------------
+  
+  if ((!isTRUE(write.file)) & is.null(x)){
+    message('No templates were written to file (write.file = FALSE).')
+  }
+  
+  message("Done.")
+  
+  if (!exists('data_read_2_x')){
+    return(x)
+  }
+  
+}
+
+
--- R/template_arguments.R
+++ R/template_arguments.R
@@ -468,6 +468,40 @@ template_arguments <- function(
       
     }
     
+    # Read annotations --------------------------------------------------------
+    
+    if (stringr::str_detect(string = templates[i], pattern = 'annotations.txt')){
+
+      if (file.exists(paste0(path, '/', templates[i]))){
+
+        output$x$template[[i]]$content <- as.data.frame(
+          data.table::fread(
+            file = paste0(path, '/', templates[i]),
+            colClasses = rep("character", 6),
+            fill = TRUE,
+            blank.lines.skip = TRUE
+          )
+        )
+
+        output$x$template[[i]]$content <- output$x$template[[i]]$content[ ,1:6]
+
+        colnames(output$x$template[[i]]$content) <- c(
+          "id",
+          "context",
+          "predicate_label",
+          "predicate_uri",
+          "object_label",
+          "object_uri"
+        )
+
+      } else {
+
+        output$x$template[[i]]$content <- NA_character_
+
+      }
+
+    }
+    
     # Read attributes (data table) --------------------------------------------
     
     if (stringr::str_detect(string = templates[i], pattern = 'attributes_.*.txt')){
