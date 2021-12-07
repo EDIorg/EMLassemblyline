a684b8b332d6285f5caa05a2a42d7c8a69a52a56
Kyle Zollo Venecek <kylezollo@gmail.com>
2021-12-07 16:26:52 -0400

    Create template raster without installed template file

--- R/template_raster_attributes.R
+++ R/template_raster_attributes.R
@@ -1,5 +1,10 @@
 template_raster_attributes <- function(
-  path = NULL, raster.files = NULL, empty = TRUE, write.file = TRUE, return.obj = FALSE) {
+  path = NULL,
+  data.path = path,
+  raster.files = NULL,
+  empty = FALSE,
+  write.file = TRUE,
+  return.obj = FALSE) {
   
   message("Templating raster attributes ...")
   
@@ -8,17 +13,55 @@ template_raster_attributes <- function(
   validate_arguments(
     fun.name = 'tempate_raster_attributes',
     fun.args = as.list(environment()))
+
+  # Create template table
   
-  # Write to file -------------------------------------------------------------
+    empty_output <- data.frame(
+       filename = character(),
+       desc = character(),
+       geoDesc = character(),
+       url = character(),
+       def = character(),
+       unit = character(),
+       numberType = character(),
+       stringsAsFactors = F
+    )
+    
+    named_output <- data.frame(
+      filename = raster.files,
+      desc = rep("", length(raster.files)),
+      geoDesc = rep("", length(raster.files)),
+      url = rep("", length(raster.files)),
+      def = rep("", length(raster.files)),
+      unit = rep("", length(raster.files)),
+      numberType = rep("", length(raster.files)),
+      stringsAsFactors = F
+    )
+    
+    write_raster_template <- function(output) {
+      suppressWarnings(
+        utils::write.table(
+          output,
+          paste0(path, "/", "raster_attributes.txt"),
+          sep = "\t",
+          row.names = F,
+          quote = F,
+          fileEncoding = "UTF-8"))
+    }
+    
+  # Write table to file -----------------------------------------------------
   
   if (!is.null(path)) {
-    invisible(
-      file.copy(
-        from = system.file(
-          "/templates/raster_attributes.txt", 
-          package = "EMLassemblyline"),
-        to = path, 
-        overwrite = FALSE))
+    
+    if (is.null(raster.files) | isTRUE(empty)) {
+      
+      # (empty)
+      write_raster_template(empty_output)  
+    
+    } else {
+      
+      write_raster_template(named_output)
+    }
   }
   
   # Return values -------------------------------------------------------------
@@ -26,11 +69,16 @@ template_raster_attributes <- function(
   message("Done.")
   
   if (isTRUE(return.obj)){
-    return(
-      data.table::fread(
-        system.file(
-          "/templates/raster_attributes.txt", 
-          package = "EMLassemblyline")))
+    
+    if (is.null(raster.files) | isTRUE(empty)) {
+      
+      # (empty)
+      return(empty_output)
+      
+    } else {
+      
+      return(named_output)
+      
+    }
   }
-  
 }
