93594a21c547ee45f0f0db0d8b686aa4ef83d7ac
Kyle Zollo Venecek <kylezollo@gmail.com>
2021-12-23 15:43:08 -0400

    Document template_spatial_attributes function

--- R/template_spatial_attribute.R
+++ R/template_spatial_attribute.R
@@ -1,3 +1,63 @@
+#' Describe spatial files
+#'
+#' @description Describes characteristics of spatial raster or spatial files. Use this template to facilitate the creation of the \code{<spatialRaster>} or \code{<spatialVector>} element in EML.
+#' 
+#' @param path 
+#'     (character) Path to the metadata template directory.
+#' @param data.path
+#'     (character) Path to the data directory.
+#' @param spatial.type
+#'     (character) Type of spatial data.
+#'     \item{Currently Supported}{
+#'         \itemize{
+#'         \item{raster: Filename of raster data}
+#'         \item{shape: Filename (without extension) of a shapefile}
+#'         }
+#'     }
+#' @param spatial.files
+#'     (character) File name. If more than one, then supply as a 
+#'     vector of character strings (e.g. 
+#'     \code{spatial.files = c('change.tif', 'mean_votes.tif')}).
+#' @param empty
+#'     (logical) Whether to write an empty template file. Default is \code{FALSE}.
+#' @param write.file
+#'     (logical; optional) Whether to write the template file. Default is \code{TRUE}.
+#' @param return.obj
+#'     (logical; optional) Whether to return the provenance template as a data frame. Default is \code{FALSE}.
+#'     
+#' @return
+#' \item{raster_attributes.txt}{Columns:
+#'     \itemize{
+#'     \item{filename: Filename of a raster (raster only)}
+#'     \item{extname: Filename without extension of a shapefile (shapefile only)}
+#'     \item{description: Description of the spatial file}
+#'     \item{geoDescription: Description of the geographic coverage of the spatial file}
+#'     \item{url: Accessible url for download of the file}
+#'     \item{definition: Definition of an attribute (raster only)}
+#'     \item{unit: Unit of an attribute (raster only)}
+#'     \item{numberType: ('real', 'natural', 'integer', 'whole', 'categorical') (raster only)}
+#'     }
+#' }
+#'
+#' @examples
+#' \dontrun{
+#' # Set working directory
+#' setwd("/Users/me/Documents/data_packages/pkg_260")
+#' 
+#' # For an empty template to be filled manually
+#' template_raster_attributes(
+#'     path = "./metadata_templates",
+#'     data.path = "./data_objects",
+#'     empty = TRUE)
+#'     
+#' # For a template with file names
+#' template_raster_attributes(
+#'     path = "./metadata_templates",
+#'     data.path = "./data_objects",
+#'     raster.files = c('change.tif', 'mean_votes.tif'))
+#' }
+#' 
+#' @export
 template_spatial_attributes <- function(
   path = NULL,
   data.path = path,
@@ -103,7 +163,4 @@ template_spatial_attributes <- function(
       
     }
   }
-  
-  
-  
 }
