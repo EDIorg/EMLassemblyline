a81e12f4a10010237d650f81f206e336140e7048
Colin Smith <colin.smith@wisc.edu>
2023-01-12 14:20:10 -0800

    Save WIP

--- R/template_data_entities.R
+++ R/template_data_entities.R
@@ -24,13 +24,43 @@ template_data_entities <- function(
     other.entity = other.entity,
   )$x
   
+  # TODO compile data_entities
+  data_entities <- something
+  
+  # TODO Data entity
+  entity_templates <- vector(mode = "list", length = data_entities)
+  names(entity_templates) <- names(data_entities)
+  for (entity in data_entities) {
+    entity_templates[[i]] <- template_data_entity()
+  }
+  
+  # TODO Data entity attributes
+  attribute_templates <- vector(mode = "list", length = data_entities)
+  names(attribute_templates) <- names(data_entities)
+  for (entity in data_entities) {
+    attribute_templates[[i]] <- template_data_entity_attributes()
+  }
+  
+  # TODO Custom units
+  custom_units_template <- vector(mode = "list", length = 1)
+  names(custom_units_template) <- "custom_units.txt"
+  custom_units_template[[1]] <- template_data_entity_custom_units()
+  
+  # TODO compile templates into a list for return
+  templates <- something
+  
+  if (write.file) {
+    write_templates(templates, names(templates), path)
+  } else {
+    return(templates)
+  }
 }
 
 
 template_data_entity <- function() {
   # TODO The superset of entity level metadata is attempted for each MIME Type.
   # Drop elements from superset that are not in the EML entity set.
-  # Return list
+  # Return data.frame
 }
 
 
@@ -41,5 +71,7 @@ template_data_entity_attributes <- function() {
 
 
 template_data_entity_custom_units <- function() {
-  # TODO Return an empty template
+  # TODO Return an empty template. Alternatively, call this function after
+  # attributes templates are completely filled out, then extract non-standard
+  # units and put them in this file.
 }
--- tests/testthat/test_template_data_entities.R
+++ tests/testthat/test_template_data_entities.R
@@ -1,7 +1,7 @@
 testthat::context("Template Data Entities")
 
 
-testthat::test_that("template_data_entity() returns list", {
+testthat::test_that("template_data_entity() returns data frame", {
   
 })
 
@@ -15,15 +15,11 @@ testthat::test_that("template_data_entity_custom_units() data frame", {
   
 })
 
-testthat::test_that("template_data_entity_custom_units() files", {
+
+testthat::test_that("template_data_entities() returns data frames", {
   
 })
 
-
-testthat::test_that("template_data_entities() data frames", {
-  
-})
-
-testthat::test_that("template_data_entities() files", {
+testthat::test_that("template_data_entities() returns files", {
   
 })
